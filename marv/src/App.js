832553b00b3e27fc2c32665cced1c4be7d09f7e3
Anna Chambers <chambaa@mail.uc.edu>
2023-02-02 14:24:34 -0500

    added simple frontend and Twitter API call

--- marv/src/App.js
+++ marv/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import SentimentAnalysis from './Components/SentimentAnalysis';
+import KeywordSearch from './Components/KeywordSearch'
+import TwitterAPI from './Components/TwitterAPI';
 import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <SentimentAnalysis/>
+      <KeywordSearch/>
     </div>
   );
 }
--- marv/src/Components/SentimentAnalysis.js
+++ marv/src/Components/SentimentAnalysis.js
@@ -1,41 +1,70 @@
 import axios from "axios";
 import React, { useState, useEffect }  from 'react';
+import data from "./testdata.json"
 
 function SentimentAnalysis() {
     const [sentimentType, setSentimentType] = useState('');
     const [text, setText] = useState('');
+    var optionsArray = [];
+    var objects = [];
 
-    const encodedParams = new URLSearchParams();
-    encodedParams.append("text", "I am so happy!");
-
-    const options = {
-    method: 'POST',
-    url: 'https://text-sentiment.p.rapidapi.com/analyze',
-    headers: {
-        'content-type': 'application/x-www-form-urlencoded',
-        'X-RapidAPI-Key': '74846fce23msh7e41880fdd46e30p10527cjsnd38522cd9765',
-        'X-RapidAPI-Host': 'text-sentiment.p.rapidapi.com'
-    },
-    data: encodedParams
-    };
+    data.results.map(tweet => {
+        var text = tweet[5]
+        const encodedParams = new URLSearchParams();
+        encodedParams.append("text", text);
+    
+        const options = {
+            method: 'POST',
+            url: 'https://text-sentiment.p.rapidapi.com/analyze',
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded',
+                'X-RapidAPI-Key': '74846fce23msh7e41880fdd46e30p10527cjsnd38522cd9765',
+                'X-RapidAPI-Host': 'text-sentiment.p.rapidapi.com'
+            },
+            data: encodedParams
+        };
+        optionsArray.push(options)
+    })
 
     useEffect(() => {
-        axios.request(options).then(function (response) {
-            setText(response.data.text);
-            if(response.data.pos > 0) {
-                setSentimentType("Positive");
-            }
-            else if(response.data.neg > 0) {
-                setSentimentType("Negative");
-            }
-            else if(response.data.mid > 0) {
-                setSentimentType("Neutral");
-            }
-        }).catch(function (error) {
-        console.error(error);
-        });
+        optionsArray.map(options => {
+            axios.request(options).then(function (response) {
+                // setText(response.data.text);
+                if(response.data.pos > 0) {
+                    // setSentimentType("Positive");
+                    var obj = {
+                        "text": response.data.text,
+                        "sentiment": "Positive"
+                    }
+                    objects.push(obj);
+                }
+                else if(response.data.neg > 0) {
+                    // setSentimentType("Negative");
+                    var obj = {
+                        "text": response.data.text,
+                        "sentiment": "Negative"
+                    }
+                    objects.push(obj);
+                }
+                else if(response.data.mid > 0) {
+                    // setSentimentType("Neutral");
+                    var obj = {
+                        "text": response.data.text,
+                        "sentiment": "Neutral"
+                    }
+                    objects.push(obj);
+                }
 
-    }, [sentimentType, text])
+            }).catch(function (error) {
+            console.error(error);
+            });
+
+        })
+        console.log(objects)
+
+    }, [sentimentType, text, optionsArray])
+
+
 
     return (
       <div className="SentimentAnalysis">
