ab05204a3d47dc7266f358201b4bfabdba3cb2ea
Anna Chambers <chambaa@mail.uc.edu>
2023-03-17 18:37:55 -0400

    Show map

--- marv/src/App.js
+++ marv/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import KeywordSearch from './Components/KeywordSearch'
 import React from 'react';
 import SentimentAnalysis from './Components/SentimentAnalysis';
+import Map from './Components/Map';
 
 function App() {
   return (
     <div className="App">
       <KeywordSearch/>
+      <Map/>
     </div>
   );
 }
