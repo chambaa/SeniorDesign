30a9f2cb8c6df0df98987914632efa3068f0b379
Veronica Ufferman <55750257+uffermvg@users.noreply.github.com>
2023-03-18 21:46:46 -0400

    Update Map.js

--- marv/src/Components/Map.js
+++ marv/src/Components/Map.js
@@ -1,5 +1,6 @@
-import React from 'react'
-// import GoogleMapReact from 'google-map-react'
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, useJsApiLoader, Marker, InfoWindow, LoadScript } from '@react-google-maps/api';
+import axios from 'axios';
 import './map.css'
 // import "https://maps.googleapis.com/maps/api/js?key=js?key=AIzaSyBgt_ybrpI0hzarHDx7Og1LkV5mS8lheQw&libraries=places";
 // const Map = props => {
@@ -56,7 +57,7 @@ import './map.css'
 //       export default Map;
 
 // import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+//import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '50%',
@@ -64,15 +65,48 @@ const containerStyle = {
 };
 
 const center = {
-  lat: -3.745,
-  lng: -38.523
+  lat: 39.103119,
+  lng: -84.512016
 };
 
-function Map() {
+
+
+const Map = props => {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
-    googleMapsApiKey: "ADD API KEY"
+    googleMapsApiKey: "AIzaSyBgt_ybrpI0hzarHDx7Og1LkV5mS8lheQw"
   })
+
+  const [stores, setStores] = useState([]);
+
+  useEffect(() => {
+    const fetchStores = async () => {
+      try {
+        const res = await axios.get(
+          `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${props.keyword}&key=${"AIzaSyBgt_ybrpI0hzarHDx7Og1LkV5mS8lheQw"}`
+        );
+        setStores(res.data.results);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchStores();
+  }, [props.keyword]);
+
+  const handleApiLoaded = (map, maps) => {
+    stores.forEach((store) => {
+      const marker = new maps.Marker({
+        position: {
+          lat: store.geometry.location.lat,
+          lng: store.geometry.location.lng
+        },
+        map,
+        title: store.name,
+        label: store.rating.toString()
+      });
+    });
+  };
 
   const [map, setMap] = React.useState(null)
 
@@ -88,13 +122,30 @@ function Map() {
     setMap(null)
   }, [])
 
+  // var request = {
+  //   query: document.getElementById('search').value,
+  //   fields: ['name', 'geometry', 'rating'],
+  // };
+
+  // service = new google.maps.places.PlacesService(map);
+
+  // service.findPlaceFromQuery(request, function(results, status) {
+  //   if (status === google.maps.places.PlacesServiceStatus.OK) {
+  //     for (var i = 0; i < results.length; i++) {
+  //       createMarker(results[i]);
+  //     }
+  //     map.setCenter(results[0].geometry.location);
+  //   }
+
   return isLoaded ? (
       <GoogleMap
         mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
+        center={stores && stores.length > 0 && stores[0].geometry && stores[0].geometry.location && { lat: stores[0].geometry.location.lat, lng: stores[0].geometry.location.lng }}
+        zoom={2.5}
         onLoad={onLoad}
         onUnmount={onUnmount}
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
       >
         { /* Child components, such as markers, info windows, etc. */ }
         <></>
